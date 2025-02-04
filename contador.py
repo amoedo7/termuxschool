578be7efff34e376da85cfd14e3e836bd3b52d34
TuNombre <tu_email@dominio.com>
2025-02-04 02:07:58 -0300

    Actualizando contador con el nuevo valor

--- contador.py
+++ contador.py
@@ -1,7 +1,19 @@
 import time
+import os
 
-counter = 0
+# Verificar si el archivo existe para leer el contador guardado
+if os.path.exists("contador.txt"):
+    with open("contador.txt", "r") as f:
+        counter = int(f.read())
+else:
+    counter = 0
+
 while True:
     print("Contador:", counter)
     counter += 1
+    
+    # Guardar el contador actualizado en el archivo
+    with open("contador.txt", "w") as f:
+        f.write(str(counter))
+    
     time.sleep(60)  # Suma 1 cada minuto
